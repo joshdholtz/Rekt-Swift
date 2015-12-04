14786ff404b964adf564f4880f4b24403402a239
Josh Holtz <josh@rokkincat.com>
2015-12-04 01:28:32 -0600

    Carthagable

---
+++ RektFramework/RektFramework.h
@@ -0,0 +1,19 @@
+//
+//  RektFramework.h
+//  RektFramework
+//
+//  Created by Josh Holtz on 12/4/15.
+//  Copyright © 2015 RokkinCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RektFramework.
+FOUNDATION_EXPORT double RektFrameworkVersionNumber;
+
+//! Project version string for RektFramework.
+FOUNDATION_EXPORT const unsigned char RektFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RektFramework/PublicHeader.h>
+
+
